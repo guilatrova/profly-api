7a8214d6665742f74e0a2077671a809c30cbf914
Guilherme Latrova <guilhermelatrova@hotmail.com>
2020-12-26 11:01:23 -0300

    feat: implement mutation for stocks

--- saveio/saveio/schema.py
+++ saveio/saveio/schema.py
@@ -6,4 +6,8 @@ class Query(savings.schema.Query, graphene.ObjectType):
     hello = graphene.String(default_value="Hi!")
 
 
-schema = graphene.Schema(query=Query)
+class Mutation(savings.schema.Mutation, graphene.ObjectType):
+    pass
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
--- saveio/savings/schema.py
+++ saveio/savings/schema.py
@@ -1,7 +1,9 @@
 import graphene
 from graphene_django import DjangoListField, DjangoObjectType
+from graphene_django.rest_framework.mutation import SerializerMutation
 
 from .models import Stock, Transaction
+from .serializers import StockSerializer
 
 
 class StockType(DjangoObjectType):
@@ -28,4 +30,14 @@ class Query(graphene.ObjectType):
         return Transaction.objects.get(pk=id)
 
 
-schema = graphene.Schema(query=Query)
+class StockMutation(SerializerMutation):
+    class Meta:
+        serializer_class = StockSerializer
+        model_operations = ["create"]
+
+
+class Mutation(graphene.ObjectType):
+    stocks = StockMutation.Field()
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
