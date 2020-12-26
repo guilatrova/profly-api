d64ef1d668a940156b46ef59447ca747f262ed3c
Guilherme Latrova <guilhermelatrova@hotmail.com>
2020-12-26 10:20:34 -0300

    feat: create stock and transaction tables

--- saveio/savings/models.py
+++ saveio/savings/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class MonetaryField(models.DecimalField):
+    def __init__(self, *args, **kwargs):
+        kwargs["max_digits"] = 19
+        kwargs["decimal_places"] = 10
+        super().__init__(*args, **kwargs)
+
+
+class Stock(models.Model):
+    name = models.CharField(max_length=100)
+    ticker = models.CharField(max_length=10)
+
+
+class Transaction(models.Model):
+    stock = models.ForeignKey(Stock, on_delete=models.PROTECT)
+    value = MonetaryField()
+    strike_price = MonetaryField()
+    units = models.IntegerField()
+
+    created_at = models.DateField(auto_now=True)
