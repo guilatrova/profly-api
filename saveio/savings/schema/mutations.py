74bfdd78671c189f345da4d277a8853a2e0a8505
Guilherme Latrova <guilhermelatrova@hotmail.com>
2020-12-31 16:15:24 -0300

    feat: add delete mutation

--- saveio/savings/schema/mutations.py
+++ saveio/savings/schema/mutations.py
@@ -1,5 +1,6 @@
 import graphene
 from graphene_django.rest_framework.mutation import SerializerMutation
+from savings.models import Transaction
 from savings.serializers import StockSerializer, TransactionSerializer
 
 
@@ -15,6 +16,19 @@ class TransactionMutation(SerializerMutation):
         model_operations = ["create"]
 
 
+class DeleteTransactionMutation(graphene.Mutation):
+    class Arguments:
+        id = graphene.ID(required=True)
+
+    ok = graphene.Boolean()
+
+    @classmethod
+    def mutate(cls, root, info, **kwargs):
+        Transaction.objects.filter(pk=kwargs["id"]).delete()
+        return cls(ok=True)
+
+
 class Mutation(graphene.ObjectType):
     stocks = StockMutation.Field()
     transactions = TransactionMutation.Field()
+    deleteTransaction = DeleteTransactionMutation.Field()
