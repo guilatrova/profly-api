9f7090d1debab147defa144d03a78d5cc227c468
Guilherme Latrova <guilhermelatrova@hotmail.com>
2021-02-25 18:35:50 -0300

    ci: enable cors only for specific domains

--- src/profly/settings/digitalocean.py
+++ src/profly/settings/digitalocean.py
@@ -4,10 +4,17 @@ from django.core.management.utils import get_random_secret_key
 
 from .debug import *
 
+# STATIC FILES
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
+# BASE
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
+# NETWORK
 ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost,*").split(",")
-
-SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
+CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_WHITELIST = (
+    "profly-i7f6f.ondigitalocean.app",
+    "https://www.profly.app",
+)
