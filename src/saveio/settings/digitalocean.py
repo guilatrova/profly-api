26c80987bb1c43e5561fc0f9d54eee2873f3f2bd
Guilherme Latrova <guilhermelatrova@hotmail.com>
2021-02-25 15:46:34 -0300

    ci: add gunicorn and additional configs to digitalocean

--- src/saveio/settings/digitalocean.py
+++ src/saveio/settings/digitalocean.py
@@ -1,7 +1,13 @@
 import os
 
+from django.core.management.utils import get_random_secret_key
+
 from .debug import *
 
-# DEBUG = False
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
-# SECRET_KEY = os.getenv("SECRET_KEY")
+DEBUG = os.getenv("DEBUG", "False") == "True"
+
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost,*").split(",")
+
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
