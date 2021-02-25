a6c2b1007a4224bffb4561f61f1a8ff8185cccfb
Guilherme Latrova <guilhermelatrova@hotmail.com>
2021-02-25 15:49:48 -0300

    refactor: rename project to profly

--- src/profly/asgi.py
+++ src/profly/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for saveio project.
+ASGI config for profly project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "saveio.settings.debug")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "profly.settings.debug")
 
 application = get_asgi_application()
