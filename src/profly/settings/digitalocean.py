3aabd3b9918e3c308de1b4436eeb1bee8ad629e5
Guilherme Latrova <guilhermelatrova@hotmail.com>
2021-02-26 10:37:26 -0300

    ci: set up postgres for digital ocean

--- src/profly/settings/digitalocean.py
+++ src/profly/settings/digitalocean.py
@@ -1,6 +1,9 @@
 import os
+import sys
 
 from django.core.management.utils import get_random_secret_key
+
+import dj_database_url
 
 from .debug import *
 
@@ -10,6 +13,16 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 # BASE
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 DEBUG = os.getenv("DEBUG", "False") == "True"
+
+# DATABASE
+if len(sys.argv) > 1 and sys.argv[1] != "collectstatic":
+    db_url = os.getenv("DATABASE_URL", None)
+    if db_url is None:
+        raise Exception("DATABASE_URL environment variable not defined")
+
+    DATABASES = {
+        "default": dj_database_url.parse(db_url),
+    }
 
 # NETWORK
 ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost,*").split(",")
