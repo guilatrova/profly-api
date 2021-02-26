45b2808ef6e9029cc00341d0b5e8006216ecd9bf
Guilherme Latrova <guilhermelatrova@hotmail.com>
2021-02-26 14:45:32 -0300

    chore: add logs to transaction creation

--- src/savings/serializers.py
+++ src/savings/serializers.py
@@ -1,3 +1,5 @@
+import logging
+
 from rest_framework import serializers
 
 from markets.services import StocksMarketService
@@ -5,6 +7,7 @@ from markets.services import StocksMarketService
 from .models import Stock, Transaction
 
 market_service = StocksMarketService()
+logger = logging.getLogger(__name__)
 
 
 class StockSerializer(serializers.ModelSerializer):
@@ -22,7 +25,10 @@ class TransactionSerializer(serializers.ModelSerializer):
     ticker = serializers.CharField(write_only=True)
 
     def validate_ticker(self, raw_ticker):
+        print("* Validating ticker")
+        logger.info(f"Validating ticker {raw_ticker}")
         stock = market_service.get_stock(raw_ticker)
+        logger.info(f"Got stock data: {stock}")
 
         if not stock:
             raise serializers.ValidationError(f"{raw_ticker} not found")
@@ -30,9 +36,11 @@ class TransactionSerializer(serializers.ModelSerializer):
         return stock
 
     def create(self, validated_data: dict):
+        logger.info(f"Creating transaction with initial data {validated_data}")
         user = self.context["request"].user
         stock = validated_data.pop("ticker")
 
         validated_data["stock_id"] = stock.id
         validated_data["user"] = user
+        logger.info(f"Augmenting data: {validated_data}")
         return super().create(validated_data)
