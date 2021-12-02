19be9e3d9689f682e7273b73036001abd3d98585
Guilherme Latrova <hello@guilatrova.dev>
2021-12-02 11:01:30 -0300

    fix: pass user agent to currency picker

--- src/currencies/adapters/yahoo.py
+++ src/currencies/adapters/yahoo.py
@@ -13,7 +13,7 @@ class YahooRateScrapper:
     def get_rate(self, from_symbol: str, to_symbol: str):
         url = f"https://finance.yahoo.com/quote/{from_symbol}{to_symbol}=X/"
         logger.info(f"Scrapping exchange rate from: {url}")
-        response = requests.get(url)
+        response = requests.get(url, headers={"User-Agent": "Python"})
 
         soup = BeautifulSoup(response.text, "lxml")
         wrapper = soup.find(id=DIV_WRAPPER_ID)
